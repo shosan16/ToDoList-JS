87e7339a577d4f6bf7dff4433313d7b58401ecc7
shota <30305829kasukabe@gmail.com>
2022-10-07 18:41:21 +0900

    ToDo追加機能の実装

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
-import "./styles.css";
+const onClickAdd = () => {
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  const li = document.createElement("li");
+  li.innerHTML = inputText;
+
+  div.appendChild(li);
+
+  document.getElementById("incomplete-list").appendChild(div);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
